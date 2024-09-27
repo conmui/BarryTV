551937050632b23ed01db30b1ada7c48a3ab9d15
conmui <175262420+conmui@users.noreply.github.com>
2024-09-26 21:23:10 -0400

    Setup Router with navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from "./pages/About";
+import Characters from "./pages/Characters";
+import Episodes from "./pages/Episodes";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">About</Link>
+            </li>
+            <li>
+              <Link to="/characters">Characters</Link>
+            </li>
+            <li>
+              <Link to="/episodes">Episodes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/characters" element={<Characters />} />
+          <Route path="/episodes" element={<Episodes />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
