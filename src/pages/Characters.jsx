e5535280d134ee0204dec0c9867c5be84014d5a8
conmui <175262420+conmui@users.noreply.github.com>
2024-09-27 21:52:29 -0400

    Character page content

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -2,17 +2,33 @@ import React from "react";
 import Title from "../components/Title";
 import Quote from "../components/Quote";
 import Logo from "../components/Logo";
+import characters from "/src/data/characters.json";
+import CharacterItem from "../components/CharacterItem";
 
 function Characters({ title }) {
   return (
     <main>
-      <Title title={"Characters"} />
-      <Quote
-        quote={"Characters quote"}
-        spokenBy={"Characters spokenBy"}
-        inEp={"Characters inEp"}
-      />
-      <Logo />
+      <section>
+        <Title title={"Characters"} />
+        <Quote
+          quote={"Characters quote"}
+          spokenBy={"Characters spokenBy"}
+          inEp={"Characters inEp"}
+        />
+      </section>
+
+      <section>
+        <ul>
+          {characters.map((item) => (
+            <CharacterItem
+              key={item.id}
+              name={item.name}
+              imgSrc={item.imgSrc}
+            />
+          ))}
+        </ul>
+        <Logo />
+      </section>
     </main>
   );
 }
