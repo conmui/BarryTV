6ce633827e73e222d35ba96d5a459e1f5316139b
conmui <175262420+conmui@users.noreply.github.com>
2024-09-28 22:37:04 -0400

    About page styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import Title from "../components/Title";
 import Quote from "../components/Quote";
 import Logo from "../components/Logo";
+import "/src/pages/About.css";
 
 function About() {
   return (
-    <main>
-      <section>
+    <main className="About">
+      <section className="About__content">
         <Title title={"About"} />
         <Quote
-          quote={"About quote"}
-          spokenBy={"About spokenBy"}
-          inEp={"About inEp"}
+          quote={"\"That's the title. They can't cut that!\""}
+          spokenBy={"Gene Cousineau"}
+          inEp={"Season 2: The Audition"}
         />
-        <p>
+        <p className="About__content-description">
           Barry is an American black comedy crime drama television series
           created by Alec Berg and Bill Hader that premiered on HBO on March 25,
           2018, and concluded on May 28, 2023, after four seasons and 32
@@ -25,14 +26,14 @@ function About() {
           with his criminal associates such as Monroe Fuches (Stephen Root) and
           NoHo Hank (Anthony Carrigan).
         </p>
-        <p>
+        <p className="About__content-description">
           Barry received critical acclaim, with most praise going to its
           directing, writing, originality, humor, characters, and performances
           (particularly those of Hader and Winkler) as well as its examination
           of its subject matter. Several critics have labeled it as one of the
           best television series of all time.
         </p>
-        <p>
+        <p className="About__content-description">
           The series has received various accolades, including 44 Primetime Emmy
           Award nominations. Hader won Outstanding Lead Actor in a Comedy Series
           twice, while Winkler won Outstanding Supporting Actor in a Comedy
@@ -48,6 +49,7 @@ function About() {
       </section>
 
       <img
+        className="About__image"
         src="/poster.jpg"
         alt="bill hader as Barry behind a stage curtain holding a gun"
       />
