89e280c7e7eddef339a8fded6e1228ae699db926
conmui <175262420+conmui@users.noreply.github.com>
2024-09-30 23:13:12 -0400

    Episode Item styles

--- src/components/EpisodeItem.jsx
+++ src/components/EpisodeItem.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "/src/components/EpisodeItem.css";
 
 function EpisodeItem({ data, onClick }) {
   return (
     <li>
-      <button onClick={onClick}>
+      <button className="EpisodeItem__btn" onClick={onClick}>
         <span>
           Season {data.season} Episode {data.episode}
         </span>
